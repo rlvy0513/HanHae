212151c8072e754c6760475622b4a3dd0da287bc
Kimseongmin <sjybext@naver.com>
2024-09-22 18:54:21 +0900

    feat: View 테스트를 위한 임시 모델 파일 추가

--- HanHae/HanHae/Models/SampleModel.swift
+++ HanHae/HanHae/Models/SampleModel.swift
@@ -5,4 +5,38 @@
 //  Created by 김성민 on 9/12/24.
 //
 
-import Foundation
+import UIKit
+
+struct ToDo {
+    let id: String = UUID().uuidString
+    var title: String
+    var note: String?
+    
+    var isCompleted: Bool = false
+    
+    var startDate: Date = Date()
+    var completionDate: Date?
+    
+    var priority: Int64 = 4
+}
+
+enum Priority: Int64 {
+    case high = 1
+    case mid
+    case low
+    case none
+}
+
+struct HHYear {
+    var year: Int
+    var months: [HHMonth]
+}
+
+struct HHMonth {
+    var year: Int
+    var month: Int
+    
+    var monthlyComment: String?
+    var toDoList: [ToDo] = []
+}
+
