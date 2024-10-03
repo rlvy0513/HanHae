67c35f13cb95456a00be9b14870a0fe2bd5caa92
Kimseongmin <sjybext@naver.com>
2024-10-03 13:07:22 +0900

    feat: SettingsViewController 파일 추가
    - View 그룹에 Settings 그룹 추가
    - SettingsViewController 파일 추가

---
+++ HanHae/HanHae/Views/Settings/SettingsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SettingsViewController.swift
+//  HanHae
+//
+//  Created by 김성민 on 10/3/24.
+//
+
+import UIKit
+
+class SettingsViewController: HHBaseViewController {
+    
+    private let tableView = UITableView(
+        frame: .zero,
+        style: .insetGrouped
+    )
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
